1c3ffdd47637ee0ca64e1d378620312dc4482822
conradhalling <conrad.halling@icloud.com>
2025-03-05 23:36:00 -0500

    feat: rename and revise script
    Rename extract_domains_for_tld.py to extract_hostnames_for_tld.py.
    Revise the script to refer to hostnames instead of domains.

--- extract_hostnames_for_tld.py
+++ extract_hostnames_for_tld.py
@@ -1,34 +1,37 @@
 r"""
 DESCRIPTION
 
-Extract the distinct domains for a given TLD and sort the values in reverse order
-by the number of hits.
+Scan cdx-nnnnn.gz files and extract the distinct hostnames for a given TLD,
+count the number of pages for each hostname.  Sort the results in descending
+order by the page_count value. Extract the domain from the hostname value. Write
+the hostname, domain, and page_count values to a CSV output file.
 
-This requires looking at the cdx-nnnnn.gz files, which are sorted alphabetically
-by TLD, to find the files containing the TLD you're interested in. Or you could
-download all the files (230 GB of .gz files) and process them all.
+To find the cdx-nnnnn.gz files containing the URLs for the desired TLD, scan
+the cluster.idx file. For data set 2024-51, the URLs for .science were in
+cdx-00278.gz.
 
 EXAMPLES
 
-python3 extract_domains_for_tld.py \
+python3 extract_hostnames_for_tld.py \
     --tld           .science \
-    --cdx_file      data/2025-08/cdx/cdx-00278.gz \
-    --out_file      data/2025-08/names/domains-science.txt \
-    --log_file      logs/extract_domains_for_tld.log \
+    --cdx_file      data/cc/2024-51/cdx-00278.gz \
+    --out_file      data/cc/2024-51/hostnames-science.csv \
+    --log_file      logs/extract_hostnames_for_tld.log \
     --log_level     debug
 
-    python3 extract_domains_for_tld.py \
+python3 extract_hostnames_for_tld.py \
     --tld           .info \
-    --cdx_file      data/2025-05/cdx/cdx-00200.gz \
-    --cdx_file      data/2025-05/cdx/cdx-00201.gz \
-    --cdx-file      data/2025-05/cdx/cdx-00202.gz \
-    --out_file      data/2025-05/names/domains-info.txt \
-    --log_file      logs/extract_domains_for_tld.log \
+    --cdx_file      data/cc/2025-05/cdx-00200.gz \
+    --cdx_file      data/cc/2025-05/cdx-00201.gz \
+    --cdx-file      data/cc/2025-05/cdx-00202.gz \
+    --out_file      data/cc/2025-05/hostnames-info.csv \
+    --log_file      logs/extract_hostnames_for_tld.log \
     --log_level     debug
 """
 
 import argparse
 import collections
+import csv
 import gzip
 import logging
 import os
@@ -36,6 +39,14 @@ import re
 import textwrap
 
 logger = logging.getLogger(__name__)
+
+
+def extract_domain_from_hostname(hostname):
+    dot_count = hostname.count(".")
+    domain = hostname
+    while domain.count(".") > 1:
+        domain = domain[domain.index(".") + 1:]
+    return domain
 
 
 def init_logging(log_file, log_level):
@@ -54,18 +65,18 @@ def parse_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="Extract domains from a Common Crawl cdx file",
-        epilog=textwrap.dedent(f"""
+        epilog=textwrap.dedent(rf"""
         Example:
-          python3 {os.path.basename(__file__)} \\
-            --tld        .science \\
-            --cdx_file   data/2025-08/cdx/cdx-00278.gz \\
-            --out_file   data/2025-08/names/domains-science.txt \\
-            --log_file   logs/extract_domains.log \\
+          python3 {os.path.basename(__file__)} \
+            --tld        .science \
+            --cdx_file   data/cc/2025-08/cdx-00278.gz \
+            --out_file   data/cc/2025-08/hostnames-science.csv \
+            --log_file   logs/extract_hostnames_for_tld.log \
             --log_level  info""")
     )
     parser.add_argument(
         "--tld",
-        help="TLD for domains to extract",
+        help="TLD for hostnames to extract",
         required=True
     )
     parser.add_argument(
@@ -76,7 +87,7 @@ def parse_args():
     )
     parser.add_argument(
         "--out_file",
-        help="output text file",
+        help="output csv file",
         required=True,
     )
     parser.add_argument(
@@ -96,21 +107,20 @@ def parse_args():
 
 def process_file(filename, pattern):
     # tld is ".info", ".science", etc.
-    domains_dict = collections.defaultdict(int)
+    hostnames_dict = collections.defaultdict(int)
     with gzip.open(filename, 'rb') as f:
         for line in f:
             match = re.search(pattern, line.decode())
             if match is not None:
-                match_string = match.group(1)
-                domains_dict[match.group(1)] += 1
-    return domains_dict
+                hostnames_dict[match.group(1)] += 1
+    return hostnames_dict
 
 
 def main():
     args = parse_args()
     init_logging(log_file=args.log_file, log_level=args.log_level)
     logger.info(__file__ + " started")
-    total_dict = dict()
+    total_hostnames_dict = dict()
     tld = args.tld
     # For my purposes, a URL ends with ":", "/", or "?", or exhausts the string,
     # which is terminated with '"'.
@@ -118,13 +128,16 @@ def main():
     logger.debug(f"pattern: {pattern}")
     for cdx_file in args.cdx_file:
         logger.info(f"  Processing {cdx_file}...")
-        file_dict = process_file(filename=cdx_file, pattern=pattern)
-        total_dict.update(file_dict)
+        hostnames_dict = process_file(filename=cdx_file, pattern=pattern)
+        total_hostnames_dict.update(hostnames_dict)
         logger.info("    Done.")
-    sorted_by_values = dict(sorted(total_dict.items(), key=lambda item: item[1], reverse=True))
+    sorted_by_values = dict(sorted(total_hostnames_dict.items(), key=lambda item: item[1], reverse=True))
     with open(args.out_file, "w") as out_f:
-        for key, value in sorted_by_values.items():
-            print("{} {}".format(key, value), file=out_f)
+        csv_writer = csv.writer(out_f)
+        csv_writer.writerow(["hostname", "domain", "page_count"])
+        for hostname, page_count in sorted_by_values.items():
+            domain = extract_domain_from_hostname(hostname)
+            csv_writer.writerow([hostname, domain, page_count])
     logger.info(__file__ + " finished")
 
 
